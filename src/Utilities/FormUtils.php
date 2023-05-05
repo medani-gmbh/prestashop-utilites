116be759fa3ba105531f86f2b1235da384d7941d
Ben Schoeffmann <benedikt.schoeffmann@medani.com>
2023-05-05 15:33:12 +0200

    Add: getDefaultHelperForm method

--- src/Utilities/FormUtils.php
+++ src/Utilities/FormUtils.php
@@ -1,6 +1,11 @@
 <?php
 namespace Medani\Prestashop\Utilities;
 
+use ModuleAdminController;
+use PrestaShop\PrestaShop\Adapter\Entity\Configuration;
+use PrestaShop\PrestaShop\Adapter\Entity\Language as EntityLanguage;
+
+
 class FormUtils {
 
     public static function getOptionsForFormSelectField(array $datas, string $idProperty = 'id', string $nameProperty = 'name') {
@@ -14,5 +19,17 @@ class FormUtils {
         }
 
         return $options;
+    }
+
+    public static function getDefaultHelperForm(ModuleAdminController $controller): HelperFormCore {
+
+        $helper = new HelperFormCore();
+        $helper->table = $controller->table;
+        $helper->identifier = $controller->identifier;
+        $lang = new EntityLanguage((int) Configuration::get('PS_LANG_DEFAULT'));
+        $helper->default_form_language = $lang->id;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        return $helper;
     }
 }
